bd6278a8994a81061eab85aa25653a5b113220cc
Taiki <taiki.iwahori.bn@furuno.co.jp>
2021-09-14 21:50:30 +0900

    ENH: 生成する関数をrandamに選ぶようにした

--- create_message.py
+++ create_message.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import random
 import numpy as np
 import plotly.express as px
 
@@ -9,14 +10,32 @@ def plot(x, y, title=None):
     return
 
 
+def create_dict_message_candidate():
+    dict_message_candidate = {
+        "y=cos(x)": np.cos,
+        "y=sin(x)": np.sin,
+        "y=tan(x)": np.tan,
+    }
+    return dict_message_candidate
+
+
+def select_dict_message(dict_message_candidate):
+    temp = list(dict_message_candidate.keys())
+    random.shuffle(temp)
+    key = temp[0]
+
+    return key, dict_message_candidate[key]
+
+
 def main():
-    message = "y = cos(x)"
+    dict_message_candidate = create_dict_message_candidate()
+    message, func = select_dict_message(dict_message_candidate)
     path_message_txt = Path("message.txt")
     with path_message_txt.open("w") as f:
         f.write(message)
 
-    x = np.arange(-np.pi, np.pi, step=0.2)
-    y = np.cos(x)
+    x = np.arange(-2 * np.pi, 2 * np.pi, step=0.02)
+    y = func(x)
     plot(x, y, title=message)
     return
 
