fe7e3c62c8ded911e311a2485a3401306fd9caa6
Taiki <taiki.iwahori.bn@furuno.co.jp>
2021-09-14 20:14:26 +0900

    画像を作成するようにした

--- create_message.py
+++ create_message.py
@@ -1,4 +1,12 @@
 from pathlib import Path
+import numpy as np
+import plotly.express as px
+
+
+def plot(x, y):
+    fig = px.line(x=x, y=y)
+    fig.write_image("image.png")
+    return
 
 
 def main():
@@ -6,6 +14,10 @@ def main():
     path_message_txt = Path("message.txt")
     with path_message_txt.open("w") as f:
         f.write(message)
+
+    x = np.arange(-np.pi, np.pi)
+    y = np.cos(x)
+    plot(x, y)
     return
 
 
