8d7983d9c45964371e681e62dc94ecb76eeec24f
Taiki <taiki.iwahori.bn@furuno.co.jp>
2021-09-14 21:36:06 +0900

    ENH: messageの変更

--- create_message.py
+++ create_message.py
@@ -3,21 +3,21 @@ import numpy as np
 import plotly.express as px
 
 
-def plot(x, y):
-    fig = px.line(x=x, y=y)
+def plot(x, y, title=None):
+    fig = px.line(x=x, y=y, title=title)
     fig.write_image("image.png")
     return
 
 
 def main():
-    message = "sample message"
+    message = "y = cos(x)"
     path_message_txt = Path("message.txt")
     with path_message_txt.open("w") as f:
         f.write(message)
 
-    x = np.arange(-np.pi, np.pi)
+    x = np.arange(-np.pi, np.pi, step=0.2)
     y = np.cos(x)
-    plot(x, y)
+    plot(x, y, title=message)
     return
 
 
