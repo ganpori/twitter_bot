5a6f88033728a363b455b0b617d41401576cae44
Taiki <taiki.iwahori.bn@furuno.co.jp>
2021-09-15 22:05:18 +0900

    ENH: 差分を描画するようにした

--- create_message.py
+++ create_message.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 import random
 import numpy as np
+import pandas as pd
 import plotly.express as px
 
 
-def plot(x, y, title=None):
-    fig = px.line(x=x, y=y, title=title)
+def plot(df, title=None):
+    fig = px.line(df, title=title)
     fig.write_image("image.png")
     return
 
@@ -29,14 +30,27 @@ def select_dict_message(dict_message_candidate):
 
 def main():
     dict_message_candidate = create_dict_message_candidate()
-    message, func = select_dict_message(dict_message_candidate)
+    fundmental_message, func = select_dict_message(dict_message_candidate)
+    h = 0.01
+    diffferential_message = fundmental_message[:-1] + "+h)/h"
+
+    message = f"{fundmental_message}, {diffferential_message}, {h=}"
+
     path_message_txt = Path("message.txt")
     with path_message_txt.open("w") as f:
         f.write(message)
 
     x = np.arange(-2 * np.pi, 2 * np.pi, step=0.02)
     y = func(x)
-    plot(x, y, title=message)
+
+    x_plus_h = x + h
+    dy_dx = (func(x_plus_h) - func(x)) / h
+
+    df = pd.DataFrame(
+        data={fundmental_message: y, diffferential_message: dy_dx},
+        index=x,
+    )
+    plot(df, title=message)
     return
 
 
